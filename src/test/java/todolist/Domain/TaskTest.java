33b419ca4a25b4f49701a06a66276432ad779f4a
Fabri Di Napoli <fabrizio.dinapoli@scmspain.com>
2016-12-09 15:13:15 +0100

    Refactor TaskTest

--- src/test/java/todolist/Domain/TaskTest.java
+++ src/test/java/todolist/Domain/TaskTest.java
@@ -1,5 +1,6 @@
 package todolist.Domain;
 
+import org.junit.Before;
 import org.junit.Test;
 import todolist.Domain.Events.TaskCreatedEvent;
 
@@ -12,35 +13,43 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TaskTest {
+
+  private static final String VALID_DESCRIPTION = "Any valid description.";
+  private static final UUID VALID_ID = UUID.randomUUID();
+  private static final String ANOTHER_VALID_DESCRIPTION = "Another valid description.";
+
+  private Task task;
+
+  @Before
+  public void setUp() {
+    this.task = Task.create(VALID_ID, VALID_DESCRIPTION);
+  }
+
   @Test
   public void itShouldRecordCreationEvent() {
-    String description = "Any valid description.";
-    UUID id = UUID.randomUUID();
-    Task task = Task.create(id, description);
 
-    assertThat(task.recordedEvents().get(0), instanceOf(TaskCreatedEvent.class));
-    assertThat(((TaskCreatedEvent) task.recordedEvents().get(0)).getId(), is(id.toString()));
-    assertThat(((TaskCreatedEvent) task.recordedEvents().get(0)).getDescription(), is(description));
-    assertThat(((TaskCreatedEvent) task.recordedEvents().get(0)).getCreatedAt(), instanceOf(Instant.class));
-    assertThat(((TaskCreatedEvent) task.recordedEvents().get(0)).getModifiedAt(), instanceOf(Instant.class));
+    TaskCreatedEvent taskCreatedEvent = (TaskCreatedEvent) task.recordedEvents().get(0);
+    
+    assertThat(taskCreatedEvent.getId(), is(VALID_ID.toString()));
+    assertThat(taskCreatedEvent.getDescription(), is(VALID_DESCRIPTION));
+    assertThat(taskCreatedEvent.getCreatedAt(), instanceOf(Instant.class));
+    assertThat(taskCreatedEvent.getModifiedAt(), instanceOf(Instant.class));
   }
 
   @Test
   public void itShouldApplyCreatedEvent() throws Exception {
-    Task task = Task.create(UUID.randomUUID(), "Any valid description.");
-    task.apply(new TaskCreatedEvent(UUID.randomUUID().toString(), "Any valid description.", Instant.now(), Instant.now()));
+    task.apply(new TaskCreatedEvent(VALID_ID.toString(), VALID_DESCRIPTION, Instant.now(), Instant.now()));
 
-    assertThat(task.getDescription(), is("Any valid description."));
+    assertThat(task.getDescription(), is(VALID_DESCRIPTION));
   }
 
   @Test
   public void itShouldUpdateDescription() throws Exception {
-    Task task = Task.create(UUID.randomUUID(), "Any valid description.");
 
     Instant oldModifiedAt = task.getModifiedAt();
-    task.updateDescription("Another valid description.");
+    task.updateDescription(ANOTHER_VALID_DESCRIPTION);
 
-    assertThat(task.getDescription(), is("Another valid description."));
+    assertThat(task.getDescription(), is(ANOTHER_VALID_DESCRIPTION));
     assertThat(task.getModifiedAt(), not(oldModifiedAt));
   }
 }
